d9c2a7f30eb07c77b38d1c1ac959b1130792fd7a
Chahid Adnane <adnanechahid@gmail.com>
2022-09-01 19:40:51 +0200

    initial commit

---
+++ src/main/java/com/adnane/tacos/web/HomeController.java
@@ -0,0 +1,20 @@
+package com.adnane.tacos.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.ServletRequest;
+import java.util.logging.Logger;
+
+@Controller
+public class HomeController {
+
+    Logger log = Logger.getLogger(HomeController.class.getName());
+
+    @GetMapping("/")
+    public String home(ServletRequest req) {
+        log.info("in home page method");
+        // This value is interpreted as the logical name of a view
+        return "home";
+    }
+}
---
+++ src/test/java/com/adnane/tacos/HomeControllerTest.java
@@ -0,0 +1,30 @@
+package com.adnane.tacos;
+
+import com.adnane.tacos.web.HomeController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void homePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(content().string(containsString("Welcome to ")));
+                //.andDo(print());
+    }
+
+}
