16a6890d725010a2ccb22cfe4dc6a98393f64ab5
Chahid Adnane <adnanechahid@gmail.com>
2022-09-18 19:29:25 +0200

    feat(): create TacoOrder model with fields validation

---
+++ src/main/java/com/adnane/tacos/model/TacoOrder.java
@@ -0,0 +1,45 @@
+package com.adnane.tacos.model;
+
+import lombok.Data;
+import org.hibernate.validator.constraints.CreditCardNumber;
+
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@Data
+public class TacoOrder {
+
+    private Long id;
+    private Date placedAt;
+    @NotBlank(message = "Delivery name is required")
+    private String deliveryName;
+    @NotBlank(message = "Street is required")
+    private String deliveryStreet;
+    @NotBlank(message = "City is required")
+    private String deliveryCity;
+    @NotBlank(message = "State is required")
+    private String deliveryState;
+    @NotBlank(message = "Zip code is required")
+    private String deliveryZip;
+    @CreditCardNumber(message = "Not a valid credit card number")
+    /**
+     * This annotation declares that the property’s value must be a valid credit card number that
+     * passes the Luhn algorithm check
+     */
+    private String ccNumber;
+    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+            message="Must be formatted MM/YY")
+    private String ccExpiration;
+    @Digits(integer=3, fraction=0, message="Invalid CVV")
+    private String ccCVV;
+
+    private List<Taco> tacos = new LinkedList<>();
+
+    public void addTaco(Taco taco){
+        this.tacos.add(taco);
+    }
+}
