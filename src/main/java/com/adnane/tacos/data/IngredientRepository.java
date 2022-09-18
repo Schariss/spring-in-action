6af45a20708bc5065b60836771b7b55cab80de9f
Chahid Adnane <adnanechahid@gmail.com>
2022-09-18 19:34:59 +0200

    feat(repository): create IngredientRepository to fetch and save ingredients

---
+++ src/main/java/com/adnane/tacos/data/IngredientRepository.java
@@ -0,0 +1,12 @@
+package com.adnane.tacos.data;
+
+import com.adnane.tacos.model.Ingredient;
+
+import java.util.Optional;
+
+public interface IngredientRepository {
+
+    Iterable<Ingredient> findAll();
+    Optional<Ingredient> findById(String id);
+    Ingredient save(Ingredient ingredient);
+}
